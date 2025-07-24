590833329d206ef2e638b4bb7df6984fcf502986
babons <sebastianbabon213@gmail.com>
2025-07-23 20:31:07 -0400

    vector addition, struct practice B)

---
+++ linear-alg/sect1-3c/vector.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+struct vector {
+	double x;
+	double y;
+	double z;
+};
+
+struct vector vectoradd(struct vector a, struct vector b) {
+	struct vector r;
+
+	r.x = (a.x + b.x);
+	r.y = (a.y + b.y);
+	r.z = (a.z + b.z);
+
+	return r;
+}
+
+struct vector makevector(double x, double y, double z) {
+	struct vector r;
+
+	r.x = x;
+	r.y = y;
+	r.z = z;
+
+	return r;
+}
+
+void printvector(struct vector p) {
+	printf("\n[%.2f, %.2f, %.2f]\n", p.x, p.y, p.z);
+}
+
+int main() {
+	struct vector a = makevector(1.0, 3.0, 5.0);
+	struct vector b = makevector(-3.0, 2.0, 7.0);
+
+	printvector(a);
+	printf("and");
+	printvector(b);
+	printf("equals");
+	struct vector sum = vectoradd(a, b);
+
+	printvector(sum);
+
+	return 0;
+}
